1bbab47897f8c339bf4ed169dfd4fcac6889e443
lsaether <Lsaether@protonmail.com>
2018-01-28 00:28:39 -0700

    added kovan

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ trigger itself every 2 hours. If the transactions do not get executed
 the canary will die. As long as the canary is up we know that the 
 alarm clock contracts are working properly. 
 
+Kovan Canary - [0xde48dcc43196a0347269eb0684da16dcf7f3f788](https://kovan.etherscan.io/address/0xde48dcc43196a0347269eb0684da16dcf7f3f788)
+
 Ropsten Canary - [0x0e6ed6ab9da0117e15bce9bc35270daeee8257cd](https://ropsten.etherscan.io/address/0x0e6ed6ab9da0117e15bce9bc35270daeee8257cd)
 
 Status Page - [Hosted on GH Pages](https://ethereum-alarm-clock.github.io/canary/)
-
--- site/index.js
+++ site/index.js
@@ -8,7 +8,12 @@ class App extends React.Component {
         this.state = {
             ropstenStatus: false,
             rCanary: {},
-            rWeb3: {}
+            rWeb3: {},
+            rLastHeartbeat: '0',
+            kovanStatus: false,
+            kCanary: {},
+            kWeb3: {},
+            kLastHeartbeat: '0',
         }
         this.getData = this.getData.bind(this)
     }
@@ -16,15 +21,44 @@ class App extends React.Component {
 
     componentDidMount () {
         const rWeb3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io'))
+        const kWeb3 = new Web3(new Web3.providers.HttpProvider('https://kovan.infura.io'))
         const canaryABI = require('./abi.json')
         const rCanary = rWeb3.eth.contract(canaryABI).at('0x0e6ed6ab9da0117e15bce9bc35270daeee8257cd')
+        const kCanary = kWeb3.eth.contract(canaryABI).at('0xde48dcc43196a0347269eb0684da16dcf7f3f788')
         // console.log(rCanary)
         rCanary.isAlive((err, initStatus) => {
             if (!err) {
                 this.setState({
                     ropstenStatus: initStatus,
                     rCanary: rCanary,
-                    rWeb3: rWeb3
+                    rWeb3: rWeb3,
+                })
+            } else {
+                console.log(err)
+            }
+        })
+        rCanary.lastHeartbeat((err, time) => {
+            if (!err) {
+                this.setState({
+                    rLastHeartbeat: time.toNumber(),
+                })
+            } else {
+                console.log(err)
+            }
+        })
+        kCanary.isAlive((err, initStatus) => {
+            if (!err) {
+                this.setState({
+                    kovanStatus: initStatus,
+                    kCanary: kCanary,
+                    kWeb3: kWeb3,
+                })
+            }
+        })
+        kCanary.lastHeartbeat((err, time) => {
+            if (!err) {
+                this.setState({
+                    kLastHeartbeat: time.toNumber(),
                 })
             } else {
                 console.log(err)
@@ -43,6 +77,33 @@ class App extends React.Component {
                 console.log(err)
             }
         })
+        this.state.kCanary.isAlive((err, status) => {
+            if (!err) {
+                this.setState({
+                    kovanStatus: status
+                })
+            } else {
+                console.log(err)
+            }
+        })
+        this.state.rCanary.lastHeartbeat((err, time) => {
+            if (!err) {
+                this.setState({
+                    rLastHeartbeat: time.toNumber(),
+                })
+            } else {
+                console.log(err)
+            }
+        })
+        this.state.kCanary.lastHeartbeat((err, time) => {
+            if (!err) {
+                this.setState({
+                    kLastHeartbeat: time.toNumber(),
+                })
+            } else {
+                console.log(err)
+            }
+        })
     }
 
     render() {
@@ -51,15 +112,26 @@ class App extends React.Component {
             <h1>Canary Status</h1>
             <button onClick={this.getData}>Refresh</button>
             <h3>
+                <a href="https://kovan.etherscan.io/address/0xde48dcc43196a0347269eb0684da16dcf7f3f788">
+                    Kovan
+                </a> - {this.state.kovanStatus ? 'alive' : 'dead'} 
+                <br />
+                Last heartbeat: {new Date(this.state.kLastHeartbeat * 1000).toLocaleString()}
+                <br />
+                <br />
+                <br />
                 <a href="https://ropsten.etherscan.io/address/0x0e6ed6ab9da0117e15bce9bc35270daeee8257cd">
                     Ropsten
-                </a> - {this.state.ropstenStatus ? 'alive' : 'dead'}</h3>
+                </a> - {this.state.ropstenStatus ? 'alive' : 'dead'}
+                <br />
+                Last heartbeat: {new Date(this.state.rLastHeartbeat * 1000).toLocaleString()}
+            </h3>
         </div>
         )
     }
 }
 
-var mountNode = document.getElementById("app")
+const mountNode = document.getElementById("app")
 ReactDOM.render(
     <App/>, 
     mountNode
